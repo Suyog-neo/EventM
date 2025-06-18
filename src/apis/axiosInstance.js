79190a92d4b7082938844e5d654cd11bed765081
Prathmesh-Neo <prathmesh.gatade@neosoftmail.com>
2025-06-18 11:22:10 +0530

    forgot pass & auth protect

--- src/apis/axiosInstance.js
+++ src/apis/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
  
 export const axiosInstance = axios.create({
-    baseURL: 'http://172.21.0.170:8000',
+    baseURL: 'http://172.21.0.215:8000',
     headers: { 'Content-Type': 'application/json' },
     withCredentials: true
 })
