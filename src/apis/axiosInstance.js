e27cc4e1b58a3ae4d773de875cdd4f8599ffa03b
Prathmesh-Neo <prathmesh.gatade@neosoftmail.com>
2025-06-20 12:41:06 +0530

    Booking history

--- src/apis/axiosInstance.js
+++ src/apis/axiosInstance.js
@@ -1,11 +1,11 @@
 import axios from "axios";
- 
+import IP_ADD from "./ip";
 export const axiosInstance = axios.create({
-    baseURL: 'http://172.21.0.206:8000',
+    baseURL: IP_ADD,
     headers: { 'Content-Type': 'application/json' },
     withCredentials: true
 })
- 
+
 axiosInstance.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem('auth');
---
+++ src/apis/ip.js
@@ -0,0 +1,2 @@
+const IP_ADD =  'http://172.21.0.194:8000';
+export default IP_ADD;
